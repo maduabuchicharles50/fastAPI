265288d1f282e0a7176ae2ad58d042a7f3604649
MbonuCharles <maduabuchicharles50@gmail.com>
2023-07-26 05:42:15 +0100

    added 3 more path

--- main.py
+++ main.py
@@ -3,6 +3,22 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# home
 @app.get("/")
 def demo():
-    return {"message": "Hello World"}
+    return {"message": "blog list"}
+
+#
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": 'all unpublished blogs'}
+
+@app.get("/blog/{id}")
+def show(id:int):
+    return {"data": id}
+
+@app.get("/blog/{id}/comments")
+def comments(id):
+    return {"data": {'1', '2'}}
+
+
